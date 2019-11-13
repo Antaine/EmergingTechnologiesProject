5ac794ca23a3d49f8f746355ee4decf8ddffcd8e
antaine <aconn@outlook.com>
2019-11-13 14:38:44 +0000

    Enviorment Setup

--- EmTechProject.py
+++ EmTechProject.py
@@ -1,1 +1,12 @@
 #Emerging Tech Project: Antaine Ó Conghaile - G00347577
+
+from flask import Flask, escape, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    return f'Hello, {escape(name)}!'
+
+print("String(hello()")
