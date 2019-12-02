e8a282b05f3b4ad2bc36ac7e70505b12d4c2db21
antaine <aconn@outlook.com>
2019-12-02 18:37:52 +0000

    Complete Setup

--- EmTechProject.py
+++ EmTechProject.py
@@ -1,6 +1,12 @@
 #Emerging Tech Project: Antaine Ó Conghaile - G00347577
 
 from flask import Flask, escape, request
+import numpy as np
+import pandas as pd
+import gzip
+
+#with gzip.open('data/t10k-images-idx3-ubyte.gz', 'rb') as f:
+ #   file_content = f.read()
 
 app = Flask(__name__)
 
