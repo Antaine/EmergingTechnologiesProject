b2e8d7023d0a488f4b73dbac4291b670a9137870
antaine <aconn@outlook.com>
2019-12-09 15:43:06 +0000

    Mnist

--- EmTechProject.py
+++ EmTechProject.py
@@ -4,6 +4,10 @@ from flask import Flask, escape, request
 import numpy as np
 import pandas as pd
 import gzip
+import tensorflow as tf
+from tensorflow import keras
+from keras.datasets import mnist
+import matplotlib.pyplot as plt
 
 #with gzip.open('data/t10k-images-idx3-ubyte.gz', 'rb') as f:
  #   file_content = f.read()
@@ -15,4 +19,6 @@ def hello():
     name = request.args.get("name", "World")
     return f'Hello, {escape(name)}!'
 
-print("String(hello()")
+print("hello")
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+class_names = ['0', '1', '2', '3', '4','5', '6', '7', '8', '9']
